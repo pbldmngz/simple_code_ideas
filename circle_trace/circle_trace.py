64f4921c1b0a659dec0fb1725130c6fd60bcd11c
Spectra <32307513+pbldmngz@users.noreply.github.com>
2020-09-15 12:34:04 -0700

    Update circle_trace.py

--- circle_trace/circle_trace.py
+++ circle_trace/circle_trace.py
@@ -4,7 +4,7 @@ import math
 pi = math.pi
 
 def circle_rel(r,n=90):
-    arr = [[250, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+    arr = [[r, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
     cir =  [[int(math.cos(2*pi/n*x)*r),int(math.sin(2*pi/n*x)*r)] for x in range(0, n+1)]
     for i in range(len(cir)-1):
         arr.append([cir[i+1][0] - cir[i][0], cir[i+1][1] - cir[i][1]])
@@ -18,4 +18,4 @@ def circle(r):
         pg.moveRel(arr[i][0], arr[i][1], duration=0.1)
 
 sleep(10)
-circle(2)
+circle(250)
